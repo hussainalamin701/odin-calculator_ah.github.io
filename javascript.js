eacc12063a994efc2f9288969e907faa58b16276
hussain.a121145 <140621473+hussainalamin701@users.noreply.github.com>
2023-09-11 17:08:04 +0100

    Modified the spacing of my buttons

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const calculator_buttons_list = document.querySelectorAll('button');
+
+const calc = document.getElementById('calc-button');
+
+function displayChange(){
+    console.log('Pressed a button');
+}
+
+
