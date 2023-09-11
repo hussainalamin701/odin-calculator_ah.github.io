52621b1a4720777ea3693b065b98a5e3217abfc1
hussain.a121145 <140621473+hussainalamin701@users.noreply.github.com>
2023-09-11 22:04:43 +0100

    Changed the display to use grid layout and added functionality to clear and delete buttons

--- javascript.js
+++ javascript.js
@@ -1,9 +1,27 @@
-const calculator_buttons_list = document.querySelectorAll('button');
+let operation = '';
 
-const calc = document.getElementById('calc-button');
+const calculator_buttons_list = document.querySelectorAll('.calc-button');
 
-function displayChange(){
-    console.log('Pressed a button');
+const calc_buttons = document.getElementById('calc-button');
+const calc_result = document.getElementById('calc-result');
+
+const calc_clear = document.getElementById('clear-button');
+const calc_delete = document.getElementById('delete-button');
+
+calculator_buttons_list.forEach((ele) => {
+    ele.onclick = () => {
+        console.log(ele.innerHTML);
+        calc_result.textContent += ele.innerHTML;
+    }
+});
+
+function clearCalculations(){
+    calc_result.innerHTML = '';
 }
 
+function deleteCalculations(){
+    calc_result.innerHTML = calc_result.innerHTML.substring(0,calc_result.innerHTML.length-1);
+}
 
+calc_clear.onclick = () => clearCalculations();
+calc_delete.onclick = () => deleteCalculations();
